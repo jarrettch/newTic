a69d02ff2bfafc332f7fd3966c7648fa5cf11510
jarrettch <jarrett.harris@gmail.com>
2013-10-04 18:07:47 -0700

    Added win conditions

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,6 +12,24 @@ angular.module('newTicApp').controller('ticTacCtrl',
   $scope.clickSqr = function(index) {
        if ($scope.board[index]) return;
        $scope.board[index] = 'OX'[turn++ % 2];
+
+       if($scope.board[0]=='X'&& $scope.board[1]=='X'&& $scope.board[2]=='X')console.log('X WINS');
+       if($scope.board[3]=='X'&& $scope.board[4]=='X'&& $scope.board[5]=='X')console.log('X WINS');
+       if($scope.board[6]=='X'&& $scope.board[7]=='X'&& $scope.board[8]=='X')console.log('X WINS');
+       if($scope.board[0]=='X'&& $scope.board[3]=='X'&& $scope.board[6]=='X')console.log('X WINS');
+       if($scope.board[1]=='X'&& $scope.board[4]=='X'&& $scope.board[7]=='X')console.log('X WINS');
+       if($scope.board[2]=='X'&& $scope.board[5]=='X'&& $scope.board[8]=='X')console.log('X WINS');
+       if($scope.board[0]=='X'&& $scope.board[4]=='X'&& $scope.board[8]=='X')console.log('X WINS');
+       if($scope.board[2]=='X'&& $scope.board[4]=='X'&& $scope.board[6]=='X')console.log('X WINS');
+
+       if($scope.board[0]=='O'&& $scope.board[1]=='O'&& $scope.board[2]=='O')console.log('O WINS!');
+       if($scope.board[3]=='O'&& $scope.board[4]=='O'&& $scope.board[5]=='O')console.log('O WINS!');
+       if($scope.board[6]=='O'&& $scope.board[7]=='O'&& $scope.board[8]=='O')console.log('O WINS!');
+       if($scope.board[0]=='O'&& $scope.board[3]=='O'&& $scope.board[6]=='O')console.log('O WINS!');
+       if($scope.board[1]=='O'&& $scope.board[4]=='O'&& $scope.board[7]=='O')console.log('O WINS!');
+       if($scope.board[2]=='O'&& $scope.board[5]=='O'&& $scope.board[8]=='O')console.log('O WINS!');
+       if($scope.board[0]=='O'&& $scope.board[4]=='O'&& $scope.board[8]=='O')console.log('O WINS!');
+       if($scope.board[2]=='O'&& $scope.board[4]=='O'&& $scope.board[6]=='O')console.log('O WINS!');
     };
   
     $scope.reset();
